d2b40517ca3c0b878ebed9fed4d5a8b53c57fbc7
Vivian Soares <vivian.soares@stone.com.br>
2024-07-15 18:34:58 -0300

    chore: first video - the settings file

---
+++ 01-the-settings-file/my-project/settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "my-project"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+    }
+    includeBuild("../my-build-logic")
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+    includeBuild("../my-other-project")
+}
+
+rootDir.listFiles()?.filter { it.isDirectory && !it.isHidden }?.forEach {
+    include(it.name)
+}
